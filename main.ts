7807621f27cc3d36be04df6182d9b156c9b16da8
ahmedmehboob811 <160405430+ahmedmehboob811@users.noreply.github.com>
2024-06-27 08:36:29 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,24 @@
+#! \usr\bin\env node 
+
+
+//npm i --save-dev @types/inquirer
+
+import inquirer from "inquirer"
+ 
+const answers :{
+    sentence:string
+} = await inquirer.prompt(
+    [
+        {
+         name:"sentence",
+         type:"input",
+         message:"Enter a sentence to count the word: "
+        }
+         
+    ]
+);
+const words = answers.sentence.trim().split("  ")
+
+console.log(words);
+
+console.log(`your sentence count word is ${words.length}`);
